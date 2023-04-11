3d04a060dbceb7262c546ae8e0957d2bba46e4de
liang.cao <liang.cao@jiduauto.com>
2023-04-11 11:47:49 +0800

    finished

--- src/operation/data_io.py
+++ src/operation/data_io.py
@@ -9,8 +9,8 @@ from src.error import ErrorWithPrompt
 
 storage_root = "./storage" if DEBUG else "/data/storage"
 """
-meta：在文件同级目录，有 "{file_name}.meta"的文件夹，存储该文件的元信息
-eg:     - {storage_root}/{email}/user_root/readme.md
+在文件同级目录，有 "{file_name}.meta"的文件夹，存储该文件的元信息
+file:   - {storage_root}/{email}/user_root/readme.md
 meta:   - {storage_root}/{email}/user_root/readme.md.meta/
  * share.txt - 创建过的分享
 
